2c78100ac932166208e04d9327c5f674768f28a1
root <root@AV-PC-L109.localdomain>
2022-09-28 10:04:56 +0200

    nothing

--- update_current_repo.sh
+++ update_current_repo.sh
@@ -14,6 +14,7 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 		# This will track any changes made to the folder on your system, since the last commit. If this is the first time you are committing the contents of the folder, it will add everything.
 		sudo git add .
 
+
 		# This will prepare the added/tracked changes to the folder on your system for pushing to Github. Here, 'insert message here' can be replaced with any relevant commit message of your choice.
 		echo "--------------------------------------------------------------------------"
 		echo "What is the commit message for this repo/code push?"
@@ -26,11 +27,24 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 		# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
 		sudo git push origin main
 		break
+	elif [ "$i" == "modified" ]; then
+		for a in $(git status | grep modified | awk '{print $2}'); do
+			echo "--------------------------------------------------------------------------"
+			echo "Any specific commit message for $a ?"
+			read commit_msg
+			sudo git commit -m "$commit_msg" "$a"
+
+			# This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
+			sudo git remote -v
+
+			# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
+			sudo git push origin main
+			break
+		done
+
 	elif [ "$i" == "nothing" ]; then
 		echo "--------------------------------------------------------------------------"
 		echo "No recent changes made to local repo - no updates to push."
-#	elif [ "$i" == "modified" ]; then
-#		for a in 
 	fi
 done
 
