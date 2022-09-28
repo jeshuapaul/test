c6bdbdda03c1137574741be51025c67784a3f4bd
root <root@AV-PC-L109.localdomain>
2022-09-28 14:00:04 +0200

    made changes to "delete" function

--- update_current_repo.sh
+++ update_current_repo.sh
@@ -30,7 +30,7 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 #			echo "--------------------------------------------------------------------------"
 #			echo "Commit message for DELETED FILE: $a ?"
 #			read commit_msg
-			sudo git commit
+			sudo git commit -m "deleted"
 #		done
 	# UNTRACKED FILES
 #	elif [ "$i" == "Untracked" ]; then
