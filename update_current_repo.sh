34bdd4bedd6a7b5a8200eac61bf1360e40c35a70
root <root@AV-PC-L109.localdomain>
2022-09-28 10:40:04 +0200

    updated

--- update_current_repo.sh
+++ update_current_repo.sh
@@ -20,12 +20,12 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 
 			# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
 			sudo git push origin main
-#	elif [ "$i" == "deleted" ] || [ "$i" == "Untracked" ] || [ "$i" == "modified" ]; then
+	elif [ "$i" == "deleted" ] || [ "$i" == "Untracked" ] || [ "$i" == "modified" ]; then
 		# This will initialize the folder/repository that you have on your local computer system.
-#		sudo git init
+		sudo git init
 
 		# This will track any changes made to the folder on your system, since the last commit. If this is the first time you are committing the contents of the folder, it will add everything.
-#		sudo git add .
+		sudo git add .
 
 		# This will prepare the added/tracked changes to the folder on your system for pushing to Github. Here, 'insert message here' can be replaced with any relevant commit message of your choice.
 #		echo "--------------------------------------------------------------------------"
@@ -35,10 +35,10 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 #				sudo git commit -m "$commit_msg"
 
 				# This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
-#				sudo git remote -v
+				sudo git remote -v
 
 				# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
-#				sudo git push origin main
+				sudo git push origin main
 #			else
 	elif [ "$i" == "nothing" ]; then
 		echo "--------------------------------------------------------------------------"
