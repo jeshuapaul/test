42c48f68630fe859b0f3984212e85163856059d9
root <root@AV-PC-L109.localdomain>
2022-09-28 10:26:36 +0200

    updated script

--- update_current_repo.sh
+++ update_current_repo.sh
@@ -7,23 +7,18 @@ cd $(pwd)
 
 # Checking if any changes have been made to the local repo and then making a decision based on that.
 for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
-
 	if [ "$i" == "modified" ]; then
 		for a in $(git status | grep modified | awk '{print $2}'); do
 			echo "--------------------------------------------------------------------------"
 			echo "Any specific commit message for $a ?"
 			read commit_msg
 			sudo git commit -m "$commit_msg" "$a"
-
 		done
 			# This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
 			sudo git remote -v
 
 			# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
 			sudo git push origin main
-			break
-
-
 	elif [ "$i" == "deleted" ] || [ "$i" == "Untracked" ] || [ "$i" == "modified" ]; then
 		# This will initialize the folder/repository that you have on your local computer system.
 		sudo git init
@@ -31,20 +26,19 @@ for i in $(sudo git status | awk '{print $1}' | cut -f1 -d ":"); do
 		# This will track any changes made to the folder on your system, since the last commit. If this is the first time you are committing the contents of the folder, it will add everything.
 		sudo git add .
 
-
 		# This will prepare the added/tracked changes to the folder on your system for pushing to Github. Here, 'insert message here' can be replaced with any relevant commit message of your choice.
-		echo "--------------------------------------------------------------------------"
-		echo "What is the commit message for this repo/code push?"
-		read commit_msg
-		sudo git commit -m "$commit_msg"
+#		echo "--------------------------------------------------------------------------"
+#		echo "What is the commit message for this repo/code push?"
+#		read commit_msg
+#			if [ "$commit_msg" == "" ];then
+#				sudo git commit -m "$commit_msg"
 
-		# This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
-		sudo git remote -v
+#				# This does some git pull and git push magic, to ensure that the contents of your new Github repository, and the folder on you local system are the same.
+#				sudo git remote -v
 
-		# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
-		sudo git push origin main
-		break
-
+				# The last word in the command 'main', is not a fixed entry when running git push. It can be replaced with any relevant 'branch_name' that you are using.
+#				sudo git push origin main
+#			else
 	elif [ "$i" == "nothing" ]; then
 		echo "--------------------------------------------------------------------------"
 		echo "No recent changes made to local repo - no updates to push."
